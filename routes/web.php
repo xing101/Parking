6e057831be6e1beb217de593063d5e8a39d63d8c
suonpisey01778 <pisey.suon@cubetiqs.com>
2024-05-07 23:31:53 +0700

    add backend

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FrontEnd\HomeController;
 use App\Http\Controllers\FrontEnd\AboutController;
 use App\Http\Controllers\FrontEnd\ContactController;
 use App\Http\Controllers\FrontEnd\ServiceController;
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home']);
@@ -11,4 +12,7 @@ Route::get('/about', [AboutController::class, 'about']);
 Route::get('/contact', [ContactController::class, 'contact']);
 Route::get('/service', [ServiceController::class, 'service']);
 
-
+//Backend 
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [DashboardController::class, 'dashboard'])->name('backend.dashboard');
+});
